0780ea337a127b76a8dcbaab732194803e350430
minguencho <cho000130@naver.com>
2021-03-09 14:43:59 +0900

    remove hello.java and added MenuManager.java

---
+++ src/mgMannegementSystem/MenuManager.java
@@ -0,0 +1,13 @@
+package mgMannegementSystem;
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("Add student");
+		System.out.println("Delete student");
+		System.out.println("Edit student");
+		System.out.println("View student");
+		System.out.println("EXIT");
+	}
+
+}
