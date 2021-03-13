9edf71011c42789ec85946e3e602ed552c26a823
minguencho <cho000130@naver.com>
2021-03-13 16:03:01 +0900

    delete ManuManager.java and added SchedulerManu.java

--- src/mgMannegementSystem/SchedulerManu.java
+++ src/mgMannegementSystem/SchedulerManu.java
@@ -1,12 +1,12 @@
 package mgMannegementSystem;
 
-public class MenuManager {
+public class SchedulerManu {
 
 	public static void main(String[] args) {
 		System.out.println("Add event");
 		System.out.println("Delete event");
-		System.out.println("Show all event");
-		System.out.println("Memo");
+		System.out.println("Edit event");
+		System.out.println("View event");
 		System.out.println("EXIT");
 	}
 
