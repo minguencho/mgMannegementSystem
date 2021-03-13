afddc6fe49ba5a40626272b6dd24b98ad23bacca
minguencho <cho000130@naver.com>
2021-03-13 15:57:54 +0900

    change contents
    Signed-off-by: minguencho <cho000130@naver.com>

--- src/mgMannegementSystem/MenuManager.java
+++ src/mgMannegementSystem/MenuManager.java
@@ -3,10 +3,10 @@ package mgMannegementSystem;
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("Add student");
-		System.out.println("Delete student");
-		System.out.println("Edit student");
-		System.out.println("View student");
+		System.out.println("Add event");
+		System.out.println("Delete event");
+		System.out.println("Show all event");
+		System.out.println("Memo");
 		System.out.println("EXIT");
 	}
 
