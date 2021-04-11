465465aeddced2e5af77656068bf5d7b304e1dbd
minguencho <80305473+minguencho@users.noreply.github.com>
2021-04-11 16:57:06 +0900

    Update Schedulermain.java

--- src/mgMannegementSystem/Schedulermain.java
+++ src/mgMannegementSystem/Schedulermain.java
@@ -16,7 +16,7 @@ public class Schedulermain {
 			System.out.println("1. Add event");
 			System.out.println("2. Delete event");
 			System.out.println("3. Edit event");
-			System.out.println("4. View event");
+			System.out.println("4. View events");
 			System.out.println("5. EXIT");
 			System.out.print("Select one number between 1 - 5 : ");
 
