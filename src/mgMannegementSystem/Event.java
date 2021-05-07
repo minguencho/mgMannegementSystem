492d2d0d1d62a6b890a3662bbd13df12b711e79e
minguencho <80305473+minguencho@users.noreply.github.com>
2021-05-07 16:51:35 +0900

    Update Event.java

--- src/mgMannegementSystem/Event.java
+++ src/mgMannegementSystem/Event.java
@@ -2,7 +2,7 @@ package Event;
 
 import java.util.Scanner;
 
-public class Event {
+public abstract class Event {
 	protected Eventskind kind = Eventskind.Assignment;
 	protected int eventnum;
 	protected String eventname;
@@ -54,6 +54,9 @@ public class Event {
 		
 		
 	}
+	public Event(Eventskind kind) {
+		this.kind  = kind;
+	}
 	
 	public Event(int num, String date) {
 		this.eventnum = num;
@@ -67,28 +70,15 @@ public class Event {
 		this.eventcontent = content;
 		
 	}
-	public void printInfo() {
-		System.out.println("Eventnumber : "+ eventnum + " Name : " + eventname + " Date : " + eventdate + " Content : " + eventcontent );
+	public Event(Eventskind kind,int num, String name, String date, String content) {
+		this.kind = kind;
+		this.eventnum = num;
+		this.eventname = name;
+		this.eventdate = date;
+		this.eventcontent = content;
+		
 	}
 	
-	public void getuserinput(Scanner input){
-		
-		System.out.println("event number");
-		int eventnum = input.nextInt();
-		this.setEventnum(eventnum);
-		
-		System.out.println("eventdate : ");
-		String eventdate = input.next();
-		this.setEventdate(eventdate);
-		
-		System.out.println("eventname : ");
-		String eventname = input.next();
-		this.setEventname(eventname);
-		
-		System.out.println("event contents : ");
-		String eventcontent = input.next();
-		this.setEventcontent(eventcontent);
-		
-	}
+	public abstract void printInfo();
 
 }
