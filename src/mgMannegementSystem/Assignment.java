8f32f2b3b83854d192b0077f0039191de13d0cd6
minguencho <80305473+minguencho@users.noreply.github.com>
2021-05-13 08:35:54 +0900

    Update Assignment.java

--- src/mgMannegementSystem/Assignment.java
+++ src/mgMannegementSystem/Assignment.java
@@ -2,64 +2,43 @@ package Event;
 
 import java.util.Scanner;
 
-public class Assignment extends Event  implements EventInput {
+import Exception.DeadlineFormatException;
+
+public class Assignment extends Event{
 	
 	public Assignment(Eventskind kind) {
 		super(kind);
 	}
-	
+
 	public void getuserinput(Scanner input){
-
-		System.out.println("event number");
-		int eventnum = input.nextInt();
-		this.setEventnum(eventnum);
-
+		setEventNum(input);
+		setEventName(input);
+		setEventContent(input);
+		setDeadLine(input);		
+	}
+	public void setDeadLine(Scanner input) {
 		char answer = 'x';
-		String deadline;
 		while(answer != 'y' && answer != 'Y' && answer != 'n' && answer != 'N' ) {
 			System.out.print("the assignment has deadline? (Y/N)");
 			answer = input.next().charAt(0);
 			if (answer == 'y' || answer == 'Y') {
-				System.out.println("deadline : today ~ deadline");
-				deadline = input.next();
-				this.setEventdate(deadline);
+				setEventDate(input);
+				setDeadline(input);
 				break;
 			}
 			else if(answer == 'n' || answer == 'N') {
-				System.out.println("eventdate (assignment): ");
-				String eventdate = input.next();
-				this.setEventdate(eventdate);
+				setEventDate(input);
+				
 				break;
 			}
 			else {
 				
 			}
 		}
-		
-		System.out.println("eventname : ");
-		String eventname = input.next();
-		this.setEventname(eventname);
-
-		System.out.println("event contents : ");
-		String eventcontent = input.next();
-		this.setEventcontent(eventcontent);
-
 	}
 	public void printInfo() {
-		String skind ="none";
-		switch(this.kind) {
-		case Assignment :
-			skind = "assignment ";
-			break;
-		case Course :
-			skind = "course";
-			break;
-		case Meeting :
-			skind = "meeting";
-			break; 
-		default : 
-			
-		}
-		System.out.println("Eventkind : "+ skind + "Eventnumber : "+ eventnum + " Name : " + eventname + " Date : " + eventdate  + " Content : " + eventcontent );
+		String skind = getKindString();
+		System.out.println("Eventkind : "+ skind + "Eventnumber : "+ eventnum + " Name : " + eventname +"Date : " + eventdate + " Deadline : "+ deadline + " Content : " + eventcontent );
 	}
 }
+
