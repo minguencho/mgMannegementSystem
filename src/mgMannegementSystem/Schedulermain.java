4a800a738a1d8dc20ac9329f7bb5084be0485868
minguencho <80305473+minguencho@users.noreply.github.com>
2021-05-13 08:38:22 +0900

    Update Schedulermain.java

--- src/mgMannegementSystem/Schedulermain.java
+++ src/mgMannegementSystem/Schedulermain.java
@@ -1,47 +1,58 @@
 package mgMannegementSystem;
 
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Schedulermain {
 
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
 		Scheduler2 scheduler = new Scheduler2(input);
-		
-		int num = -1;
-		
-		while (num != 5) {
-			System.out.println("1. Add event");
-			System.out.println("2. Delete event");
-			System.out.println("3. Edit event");
-			System.out.println("4. View events");
-			System.out.println("5. EXIT");
-			System.out.print("Select one number between 1 - 5 : ");
 
-			num = input.nextInt();
-
-			if(num ==1) {
-				scheduler.addEvent();
-			}
-			else if(num ==2) {
-				scheduler.deleteEvent();
-
-			}
-			else if(num ==3) {
-				scheduler.editEvent();
-			}
-			else if(num ==4) {
-				scheduler.viewEvent();
-			}
-			else {
-				continue;
-
-			}
-		}
+		selectMenu(input,scheduler);
 	}
 
-	
+	public static void selectMenu(Scanner input, Scheduler2 scheduler) {
+		int num = -1;
+		while (num != 5) {
+			try {
+				showMenu();
+				num = input.nextInt();
+				switch(num) {
+				case 1 :
+					scheduler.addEvent();
+					break;
+				case 2 :
+					scheduler.deleteEvent();
+					break;
+				case 3 :
+					scheduler.editEvent();
+					break;
+				case 4 :
+					scheduler.viewEvent();
+					break;
+				default :
+					continue;	
+				}
+			}
+			catch(InputMismatchException e) {
+				System.out.println("please put an Integer between 1 and 5 ");
+				if(input.hasNext()) {
+					input.next();
+				}
+				num = -1;
+			}
+
+		}
+	}
+	public static void showMenu() {
+		System.out.println("1. Add event");
+		System.out.println("2. Delete event");
+		System.out.println("3. Edit event");
+		System.out.println("4. View events");
+		System.out.println("5. EXIT");
+		System.out.print("Select one number between 1 - 5 : ");
+	}
 }
-	
