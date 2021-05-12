a1461ee2f8da3bde5bcd046b323a10b4ef23fedd
minguencho <80305473+minguencho@users.noreply.github.com>
2021-05-13 08:38:41 +0900

    Update meeting.java

--- src/mgMannegementSystem/meeting.java
+++ src/mgMannegementSystem/meeting.java
@@ -2,7 +2,7 @@ package Event;
 
 import java.util.Scanner;
 
-public class Meeting extends Event implements EventInput {
+public class Meeting extends Event{
 	protected String place;
 
 	public Meeting(Eventskind kind) {
@@ -11,48 +11,20 @@ public class Meeting extends Event implements EventInput {
 
 	public void getuserinput(Scanner input){
 
-		System.out.println("event number");
-		int eventnum = input.nextInt();
-		this.setEventnum(eventnum);
+		setEventNum(input);
 		
-		
-		
-
 		System.out.println("where do you meet?");
 		place = input.next();
 
-		System.out.println("eventdate : ");
-		String eventdate = input.next();
-		this.setEventdate(eventdate);
+		setEventDate(input);
+			
+		setEventName(input);
 		
-		
-		
-		System.out.println("eventname : ");
-		String eventname = input.next();
-		this.setEventname(eventname);
-		
-		
-		
-		System.out.println("event contents : ");
-		String eventcontent = input.next();
-		this.setEventcontent(eventcontent);
+		setEventContent(input);
 
 	}
 	public void printInfo() {
-		String skind ="none";
-		switch(this.kind) {
-		case Assignment :
-			skind = "assignment ";
-			break;
-		case Course :
-			skind = "course";
-			break;
-		case Meeting :
-			skind = "meeting";
-			break; 
-		default : 
-			
-		}
+		String skind = getKindString();
 		System.out.println("Eventkind : "+ skind + "Eventnumber : "+ eventnum + " Name : " + eventname + " Date : " + eventdate + " Content : " + eventcontent + " meeting place : "+ place );
 	}
 }
