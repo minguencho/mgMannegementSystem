543cb9b23e9c1f931af72b344b83478fce7b42a4
minguencho <80305473+minguencho@users.noreply.github.com>
2021-05-13 08:37:54 +0900

    Update Scheduler2.java

--- src/mgMannegementSystem/Scheduler2.java
+++ src/mgMannegementSystem/Scheduler2.java
@@ -1,6 +1,7 @@
 package mgMannegementSystem;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import Event.Assignment;
@@ -11,119 +12,129 @@ import Event.Eventskind;
 import Event.Meeting;
 
 public class Scheduler2 {
-	ArrayList<EventInput> event = new ArrayList<EventInput>();
+	ArrayList<EventInput> events = new ArrayList<EventInput>();
 	Scanner input;
-	
+
 	Scheduler2(Scanner input){
 		this.input = input;
-		
+
 	}
 	public void addEvent() {
 		int kind = 0;
-		
+
 		EventInput eventsInput;
-		
-		while(kind != 1 && kind != 2) {
-		System.out.println("Select Event kind : ");
-		System.out.println("1 for Assignment ");
-		System.out.println("2 for Course ");
-		System.out.println("3 for Meeting ");
-		System.out.println("Select num 1,2 or 3 for Event kind : ");
-		kind = input.nextInt();
-		if (kind == 1) {
-			eventsInput = new Assignment(Eventskind.Assignment);
-			eventsInput.getuserinput(input);
-			event.add(eventsInput);
-			break;
-		}
-		else if (kind == 2) {
-			eventsInput = new Course(Eventskind.Course);
-			eventsInput.getuserinput(input);
-			event.add(eventsInput);
-			break;
-		
-		}
-		else if (kind == 3) {
-			eventsInput = new Meeting(Eventskind.Meeting);
-			eventsInput.getuserinput(input);
-			event.add(eventsInput);
-			break;
-		
-		}
-		
-		else {
-			System.out.println("Select num 1,2 or 3 for Event kind : ");
+
+		while(kind < 1 || kind > 3) {
+			try {
+				System.out.println("Select Event kind : ");
+				System.out.println("1 for Assignment ");
+				System.out.println("2 for Course ");
+				System.out.println("3 for Meeting ");
+				System.out.println("Select num 1,2 or 3 for Event kind : ");
+				kind = input.nextInt();
+				if (kind == 1) {
+					eventsInput = new Assignment(Eventskind.Assignment);
+					eventsInput.getuserinput(input);
+					events.add(eventsInput);
+					break;
+				}
+				else if (kind == 2) {
+					eventsInput = new Course(Eventskind.Course);
+					eventsInput.getuserinput(input);
+					events.add(eventsInput);
+					break;
+
+				}
+				else if (kind == 3) {
+					eventsInput = new Meeting(Eventskind.Meeting);
+					eventsInput.getuserinput(input);
+					events.add(eventsInput);
+					break;
+
+				}
+
+				else {
+					System.out.println("Select num 1,2 or 3 for Event kind : ");
+				}
 			}
+			catch(InputMismatchException e) {
+				System.out.println("please put an Integer between 1 and 3 ");
+				if(input.hasNext()) {
+					input.next();
+				}
+				kind = -1;
+			}	
 		}
-	
 	}
+
+
 	
 	public void deleteEvent() {
 		System.out.print("Event Number : ");
 		int eventnum = input.nextInt();
+		int index = findIndex(eventnum);
+		removefromStudents(index , eventnum);	
+	}
+	
+	public int findIndex(int eventnum) {
 		int index = -1;
-		for (int i =0; i<event.size();i++) {
-			if (event.get(i).getEventnum() == eventnum) {
+		for (int i =0; i<events.size();i++) {
+			if (events.get(i).getEventnum() == eventnum) {
 				index = i;
 				break;
 			}
-		}
-		
+		} 
+		return index;
+	}
+	
+	public int removefromStudents(int index ,int eventnum) {
 		if(index >= 0) {
-			event.remove(index);
-			
+			events.remove(index);
 			System.out.println("event "+eventnum+" is deleted");
+			return 1;
 		}
 		else {
 			System.out.println("event has not been registered");
-			return;
+			return -1;
 		}
 	}
 	
 	public void editEvent() {
 		System.out.print("Event Number : ");
 		int eventnum = input.nextInt();
-		for (int i = 0; i<event.size();i++) {
-			EventInput eventsInput = event.get(i);
-			if(eventsInput.getEventnum() == eventnum) {
+		for (int i = 0; i<events.size();i++) {
+			EventInput event = events.get(i);
+			if(event.getEventnum() == eventnum) {
 				int num = -1;
 				System.out.print("EDIT : 1.Event date 2.Event name 3. Event content ");
 				
 				while (num < 4) {
 				
 					num = input.nextInt();
-				
-					if(num ==1) {
-						
-						System.out.print("Event date");
-						String eventdate = input.next();
-						eventsInput.setEventdate(eventdate);
-					}
-					else if(num ==2) {
-						System.out.print("Event name");
-						String eventname = input.next();
-						eventsInput.setEventname(eventname);
-					}
-					else if(num ==3) {
-						System.out.print("Event content");
-						String eventcontent = input.next();
-						eventsInput.setEventcontent(eventcontent);
-					}	
-					else if(num >=4 ) {
+					switch(num) {
+					case 1:
+						event.setEventDate(input);
 						break;
-						
+					case 2:
+						event.setEventName(input);
+						break;
+					case 3:
+						event.setEventContent(input);
+						break;
+					default:
+						continue;
 					}
-			
-				}
 				break;
+				}
 			}
-			
 		}
 	}
+		
 	public void viewEvent() {
-		for (int i=0; i < event.size(); i++){
-			event.get(i).printInfo();
+		for (int i=0; i < events.size(); i++){
+			events.get(i).printInfo();
 		}
 	}
-
+	
+	
 }
