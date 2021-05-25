6ea8a37e65476466b6d3841ad57fcbb9202d5514
minguencho <80305473+minguencho@users.noreply.github.com>
2021-05-25 21:01:50 +0900

    Create EventsAdder.java

---
+++ src/mgMannegementSystem/EventsAdder.java
@@ -0,0 +1,50 @@
+package GUI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class EventsAdder extends JFrame {
+	public EventsAdder() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new SpringLayout());
+		
+		JLabel labelID  = new JLabel("Eventnumber : ", JLabel.TRAILING);
+		JTextField feildID = new JTextField(10);
+		labelID.setLabelFor(feildID);
+		panel.add(labelID);
+		panel.add(feildID);
+		
+		JLabel labelName  = new JLabel("Name : ", JLabel.TRAILING);
+		JTextField feildName = new JTextField(10);
+		labelName.setLabelFor(feildName);
+		panel.add(labelName);
+		panel.add(feildName);
+
+		
+		JLabel labelDate  = new JLabel("Date : ", JLabel.TRAILING);
+		JTextField feildDate = new JTextField(10);
+		labelDate.setLabelFor(feildDate);
+		panel.add(labelDate);
+		panel.add(feildDate);
+		
+		JLabel labelContent  = new JLabel("Content : ", JLabel.TRAILING);
+		JTextField feildContent = new JTextField(10);
+		labelContent.setLabelFor(feildContent);
+		panel.add(labelContent);
+		panel.add(feildContent);
+		
+		panel.add(new JButton("save"));
+		panel.add(new JButton("cancel"));
+		
+		SpringUtilities.makeCompactGrid(panel, 5, 2, 6, 6, 6, 6);
+		
+		this.setSize(300,300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setContentPane(panel);
+		this.setVisible(true);
+	}
+}
