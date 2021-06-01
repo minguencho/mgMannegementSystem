b9d6ad789c9efb97eb6965bdbae6efe35363081c
minguencho <80305473+minguencho@users.noreply.github.com>
2021-06-01 16:21:33 +0900

    Update EventViewer.java

--- src/mgMannegementSystem/EventViewer.java
+++ src/mgMannegementSystem/EventViewer.java
@@ -1,25 +1,45 @@
 package GUI;
 
+import java.util.Vector;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-public class EventViewer extends JFrame {
-	public EventViewer() {
+import Event.EventInput;
+import mgMannegementSystem.Scheduler2;
+
+public class EventViewer extends JPanel {
+	
+	WindowFrame frame;
+	
+	Scheduler2 scheduler;
+	
+	public EventViewer(WindowFrame frame,Scheduler2 scheduler) {
+		this.frame = frame;
+		this.scheduler = scheduler;
+		System.out.println("**"+scheduler.size() + "**");
+		
 		DefaultTableModel model = new DefaultTableModel();
 		model.addColumn("NUM");
 		model.addColumn("Name");
 		model.addColumn("Date");
 		model.addColumn("Content");
-
+		for(int i= 0; i<scheduler.size(); i++) {
+			Vector row = new Vector();
+			EventInput ei = scheduler.get(i);
+			row.add(ei.getEventnum());
+			row.add(ei.getEventname());
+			row.add(ei.getEventdate());
+			row.add(ei.getEventcontent());
+			row.add(ei.getdeadline());
+			model.addRow(row);
+		}
 		JTable table = new JTable(model);;
 		JScrollPane sp = new JScrollPane(table);
 		
 		this.add(sp);
-		this.setSize(300,300);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		this.setVisible(true);
 	}
 }
