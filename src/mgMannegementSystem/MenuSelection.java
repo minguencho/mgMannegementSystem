f8c18881e65ec3c2846f026eb9434366ff64b0f7
minguencho <80305473+minguencho@users.noreply.github.com>
2021-06-01 16:22:09 +0900

    Update MenuSelection.java

--- src/mgMannegementSystem/MenuSelection.java
+++ src/mgMannegementSystem/MenuSelection.java
@@ -7,12 +7,14 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class MenuSelection extends JFrame{
-	
-	public MenuSelection() {
-		this.setSize(300,300);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+import Listener.ButtonAddListener;
+import Listener.ButtonViewListener;
+
+public class MenuSelection extends JPanel{
+	WindowFrame frame;
+	public MenuSelection(WindowFrame frame) {
+		this.frame = frame;
+		this.setLayout(new BorderLayout());
 		JPanel panel1 = new JPanel();
 		JPanel panel2 = new JPanel();
 		JLabel label = new JLabel("Menu Selection");
@@ -22,6 +24,8 @@ public class MenuSelection extends JFrame{
 		JButton button3 = new JButton("Edit Event");
 		JButton button4 = new JButton("Delete Event");
 		JButton button5 = new JButton("EXIT Program");
+		button1.addActionListener(new ButtonAddListener(frame));
+		button2.addActionListener(new ButtonViewListener(frame));
 		
 		panel1.add(label);
 		panel2.add(button1);
@@ -30,10 +34,8 @@ public class MenuSelection extends JFrame{
 		panel2.add(button4);
 		panel2.add(button5);
 		
-		
 		this.add(panel1,BorderLayout.NORTH);
 		this.add(panel2,BorderLayout.CENTER);
-		this.setVisible(true);
-		
+
 	}
 }
