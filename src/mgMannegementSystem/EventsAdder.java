b823ef9a9c31e4d1d460e97ff45d84340a86e265
minguencho <80305473+minguencho@users.noreply.github.com>
2021-06-01 16:21:47 +0900

    Update EventsAdder.java

--- src/mgMannegementSystem/EventsAdder.java
+++ src/mgMannegementSystem/EventsAdder.java
@@ -7,8 +7,13 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
-public class EventsAdder extends JFrame {
-	public EventsAdder() {
+public class EventsAdder extends JPanel {
+	
+	WindowFrame frame;
+	
+	public EventsAdder(WindowFrame frame) {
+		this.frame = frame;
+		
 		JPanel panel = new JPanel();
 		panel.setLayout(new SpringLayout());
 		
@@ -23,7 +28,6 @@ public class EventsAdder extends JFrame {
 		labelName.setLabelFor(feildName);
 		panel.add(labelName);
 		panel.add(feildName);
-
 		
 		JLabel labelDate  = new JLabel("Date : ", JLabel.TRAILING);
 		JTextField feildDate = new JTextField(10);
@@ -36,15 +40,10 @@ public class EventsAdder extends JFrame {
 		labelContent.setLabelFor(feildContent);
 		panel.add(labelContent);
 		panel.add(feildContent);
-		
 		panel.add(new JButton("save"));
 		panel.add(new JButton("cancel"));
-		
 		SpringUtilities.makeCompactGrid(panel, 5, 2, 6, 6, 6, 6);
-		
-		this.setSize(300,300);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setContentPane(panel);
+		this.add(panel);
 		this.setVisible(true);
 	}
 }
